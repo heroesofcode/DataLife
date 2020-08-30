e5e46fdcedc26fe28761b61984224f3ffe51cfbe
João Lucas <joaolucasfp2001@gmail.com>
2020-08-29 23:37:07 -0300

    Add constructor in DataLifeViewModel

--- Sources/DataLife/DataLifeViewModel.swift
+++ Sources/DataLife/DataLifeViewModel.swift
@@ -9,6 +9,8 @@ import Foundation
 
 open class DataLifeViewModel: ObserverProtocol {
     public var id: Int = 0
+    
+    public init() {}
 
     public func onValueChanged(_ value: Any?) {
         //empty
