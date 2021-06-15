ebd0e49634555b664c59136474829b2685a90c44
João Lucas <joaolucasfp2001@gmail.com>
2021-06-14 22:10:24 -0300

    Removed protocol that was not being used

--- Sources/DataLife/ObserverProtocol.swift
+++ Sources/DataLife/ObserverProtocol.swift
@@ -11,10 +11,3 @@ public protocol ObserverProtocol {
     var id : Int { get set }
     func onValueChanged(_ value: Any?)
 }
-
-protocol DataLifeProtocol : class {
-    var observers : [DataLifeProtocol] { get set }
-
-    func observer(_ observer: ObserverProtocol)
-    func notifyObservers(_ observers: [ObserverProtocol])
-}
