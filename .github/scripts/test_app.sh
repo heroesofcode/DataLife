c77cfd489d8ff41cf5f3f0471a748056852ad15c
João Lucas <joaolucasfp2001@gmail.com>
2021-09-10 15:34:41 -0300

    Fix and back CI

--- .github/scripts/test_app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eo pipefail
-
-xcodebuild -project DataLife.xcodeproj \
-           -scheme DataLife-Package \
-           -destination platform=iOS\ Simulator,OS=14.4,name=iPhone\ 8 \
-           clean test | xcpretty
