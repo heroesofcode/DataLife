893baeeabddda82a572eff7e8858d66ee71bc7a6
João Lucas <joaolucasfp2001@gmail.com>
2021-09-10 15:28:08 -0300

    Update test_app.sh

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 xcodebuild -project DataLife.xcodeproj \
            -scheme DataLife-Package \
-           -destination platform=iOS\ Simulator,name=iPhone\ 8 \
-           clean test
+           -destination platform=iOS\ Simulator,OS=14.4,name=iPhone\ 8 \
+           clean test | xcpretty
