47e3729acbc78bd777a5b1135bae918c947cfd73
João Lucas <joaolucasfp2001@gmail.com>
2021-09-10 15:11:04 -0300

    Fix permission in script

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -5,4 +5,4 @@ set -eo pipefail
 xcodebuild -project DataLife.xcodeproj \
            -scheme DataLife-Package \
            -destination platform=iOS\ Simulator,name=iPhone\ 8 \
-           clean test | xcpretty
+           clean test
