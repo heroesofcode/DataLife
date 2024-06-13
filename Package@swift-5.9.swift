2dd7605c128718fdf57b3ea41e8aaed64ec99bf6
joaolfp <joaolucasfp2001@gmail.com>
2024-06-13 01:23:40 -0300

    Add support to visionOS

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DataLife",
-    platforms: [.iOS(.v12), .macOS(.v10_13), .tvOS(.v12), .watchOS(.v4)],
+    platforms: [.iOS(.v12), .macOS(.v10_13), .tvOS(.v12), .watchOS(.v4), .visionOS(.v1)],
     products: [
         .library(
             name: "DataLife",
