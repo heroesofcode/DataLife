85e9ae785ec7d5951ccfe9d892072fc5d3ef3db8
joaolfp <joaolucasfp2001@gmail.com>
2024-06-13 00:51:43 -0300

    Add Arena Playground with module

---
+++ Arena-Playground/Content.playground/Contents.swift
@@ -0,0 +1,27 @@
+// Playground generated with 🏟 Arena (https://github.com/finestructure/arena)
+// ℹ️ If running the playground fails with an error "No such module"
+//    go to Product -> Build to re-trigger building the SPM package.
+// ℹ️ Please restart Xcode if autocomplete is not working.
+
+import DataLife
+
+final class ViewModel: DataLifeViewModel {
+    
+    var myName = DataLife<String>()
+    
+    func getMyName() {
+        myName.value = "João Lucas"
+    }
+}
+
+var getName = ""
+
+let viewModel = ViewModel()
+
+viewModel.myName.observer(viewModel) { name in
+    getName = name
+}
+
+viewModel.getMyName()
+
+print("\(getName)")
